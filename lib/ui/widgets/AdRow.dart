d42d0df8532b6e8e5b1c4d92c17d7c070d7a2a3d
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-16 14:52:38 +0430

    image shows correctly

--- lib/ui/widgets/AdRow.dart
+++ lib/ui/widgets/AdRow.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:linkish/ui/widgets/CustomVideoPlayer.dart';
 
 import '../../models/Ad.dart';
 
@@ -10,12 +9,12 @@ class AdRow extends StatefulWidget {
 }
 
 class _AdRowState extends State<AdRow> {
+  late Image _image;
   late Ad _ad;
 
   getAds() {
     _ad = new Ad("New Ad", "https://www.google.com", false,
-        "https://i.dlpng.com/static/png/6478590_preview.png",
-        ["sport", "news"]);
+        "http://192.168.1.9:8000/ad/media/soori_land.jpg", ["sport", "news"]);
   }
 
   showPreviousAd() {}
@@ -25,12 +24,16 @@ class _AdRowState extends State<AdRow> {
   @override
   void initState() {
     this.getAds();
+    _image = Image.network("https://www.bruna.nl/images/active/carrousel/fullsize/9789492934468_front.jpg");
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
     return Container(
       child: Column(
         children: [
@@ -44,16 +47,40 @@ class _AdRowState extends State<AdRow> {
               ),
             ),
           ),
-          Row(
-            children: [
-              IconButton(onPressed: showPreviousAd, icon: Icon(Icons.chevron_left), iconSize: 40,),
-              Container(
-                width: width * 0.65,
-                child: CustomVideoPlayer(),
-              ),
-              IconButton(onPressed: showNextAd, icon: Icon(Icons.chevron_right), iconSize: 40,),
-            ],
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: showPreviousAd,
+                  icon: Icon(Icons.chevron_left),
+                  iconSize: 40,
+                ),
+                Container(
+                  height: height * 0.6,
+                  width: width * 0.65,
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: _image,
+                ),
+                IconButton(
+                  onPressed: showNextAd,
+                  icon: Icon(Icons.chevron_right),
+                  iconSize: 40,
+                ),
+              ],
+            ),
           ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: () {}, child: Text("Confirm"), style: buttonStyle,),
+                ElevatedButton(onPressed: () {}, child: Text("Reject"), style: buttonStyle),
+                ElevatedButton(onPressed: () {}, child: Text("Report"), style: buttonStyle),
+              ],
+            ),
+          )
         ],
       ),
     );
