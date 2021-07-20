7b65b18a74034469b567583b90f731d640c4c915
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-20 08:43:13 +0430

    implement 2 methods in Topic class. toMap() and a static method mapToTopic. to use for converting to json and adding in sharedpreferences

--- lib/models/topic.dart
+++ lib/models/topic.dart
@@ -1,4 +1,3 @@
-
 class Topic {
   int _id;
   String _title;
@@ -7,28 +6,16 @@ class Topic {
 
   String get title => _title;
   int get id => _id;
-}
 
-class TopicList {
-  List<Topic> _topics = [];
-
-  List<Topic> get topics => _topics;
-  set topics(List<Topic> value) {
-    _topics = value;
-  }
-  void addTopic(Topic topic) {
-    _topics.add(topic);
+  Map toMap() {
+    Map map = {
+      'id': this._id,
+      'title': this._title,
+    };
+    return map;
   }
 
-  List<int> getTopicsId(List<String> topicsTitle) {
-    List<int> list = [];
-    List<String> allTopicsTitles = List.generate(_topics.length, (index) => _topics[index].title);
-
-    topicsTitle.forEach((element) {
-      int index = allTopicsTitles.indexOf(element);
-      list.add(_topics[index].id);
-    });
-
-    return list;
+  static Topic mapToTopic(Map map) {
+    return Topic(map['id'], map['title']);
   }
 }
