2482eecad496fc35e811cdff048fed1bedd69324
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-20 08:44:48 +0430

    some ui changes in TopicChip.dart and now it has an empty onPressed method

--- lib/ui/widgets/TopicChip.dart
+++ lib/ui/widgets/TopicChip.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 class TopicChip extends StatelessWidget {
   final String label;
+
   TopicChip(this.label);
 
   @override
   Widget build(BuildContext context) {
-    return Chip(
+    return InputChip(
       labelPadding: EdgeInsets.all(2.0),
       avatar: CircleAvatar(
         backgroundColor: Colors.white70,
@@ -22,6 +23,16 @@ class TopicChip extends StatelessWidget {
       elevation: 6.0,
       shadowColor: Colors.grey[60],
       padding: EdgeInsets.all(8.0),
+      onDeleted: () {
+        removeItem();
+      },
+      onPressed: () {
+        removeItem();
+      },
     );
+  }
+
+  removeItem() {
+    print("hellp");
   }
 }
