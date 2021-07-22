42aeb331619ed7d55a07f91b614c48d43f3face5
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-22 21:43:42 +0430

    some ui changes in AdRow.dart

--- lib/ui/widgets/AdRow.dart
+++ lib/ui/widgets/AdRow.dart
@@ -10,6 +10,7 @@ class AdRow extends StatefulWidget {
 }
 
 class _AdRowState extends State<AdRow> {
+  bool _isLoading = true;
   List<Ad> _adsList = [];
   late Ad _ad;
   bool _hasNext = false;
@@ -19,16 +20,43 @@ class _AdRowState extends State<AdRow> {
     List<Ad> _list = await WebApi().getDisapprovedAds(3);
     setState(() {
       this._adsList = _list;
-      this._ad = _adsList.first;
-      if (_adsList.last != _ad) {
-        this._hasNext = true;
+
+      if (_list.length > 0) {
+        this._ad = _adsList.first;
+        if (_adsList.last != _ad) {
+          this._hasNext = true;
+        }
+      }
+
+      _isLoading = false;
+    });
+  }
+
+  showPreviousAd() {
+    int index = _adsList.indexOf(_ad);
+    setState(() {
+      _ad = _adsList[index - 1];
+      _hasNext = true;
+      if (_adsList.first == _ad) {
+        _hasBefore = false;
+      } else {
+        _hasBefore = true;
       }
     });
   }
 
-  showPreviousAd() {}
-
-  showNextAd() {}
+  showNextAd() {
+    int index = _adsList.indexOf(_ad);
+    setState(() {
+      _ad = _adsList[index + 1];
+      _hasBefore = true;
+      if (_adsList.last == _ad) {
+        _hasNext = false;
+      } else {
+        _hasNext = true;
+      }
+    });
+  }
 
   @override
   void initState() {
@@ -44,69 +72,73 @@ class _AdRowState extends State<AdRow> {
         ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
 
     return Container(
-      child: _adsList.isEmpty
+      child: _isLoading
           ? Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(14.0),
-                  child: Text(
-                    this._ad.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+          : _adsList.isEmpty
+              ? Center(child: Text("No Suggested Ads", style: TextStyle(fontSize: 24),))
+              : Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: Text(
+                        this._ad.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
-                  ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            // right button
+                            onPressed: _hasBefore ? showPreviousAd : null,
+                            icon: Icon(Icons.chevron_left),
+                            iconSize: 40,
+                            color: _hasBefore ? Colors.black : Colors.grey,
+                          ),
+                          Container(
+                            height: height * 0.6,
+                            width: width * 0.65,
+                            decoration: BoxDecoration(border: Border.all()),
+                            child: Image.network(
+                                "${WebApi.baseUrl}${_ad.imageUrl}"),
+                          ),
+                          IconButton(
+                            // left button
+                            onPressed: _hasNext ? showNextAd : null,
+                            icon: Icon(Icons.chevron_right),
+                            iconSize: 40,
+                            color: _hasNext ? Colors.black : Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Confirm"),
+                            style: buttonStyle,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Reject"),
+                              style: buttonStyle),
+                          ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Report"),
+                              style: buttonStyle),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton( // right button
-                        onPressed: _hasBefore ? showPreviousAd : null,
-                        icon: Icon(Icons.chevron_left),
-                        iconSize: 40,
-                        color: _hasBefore ? Colors.black : Colors.grey,
-                      ),
-                      Container(
-                        height: height * 0.6,
-                        width: width * 0.65,
-                        decoration: BoxDecoration(border: Border.all()),
-                        child:
-                            Image.network("${WebApi.baseUrl}${_ad.imageUrl}"),
-                      ),
-                      IconButton( // left button
-                        onPressed: _hasNext ? showNextAd : null,
-                        icon: Icon(Icons.chevron_right),
-                        iconSize: 40,
-                        color: _hasNext ? Colors.black : Colors.grey,
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text("Confirm"),
-                        style: buttonStyle,
-                      ),
-                      ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Reject"),
-                          style: buttonStyle),
-                      ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Report"),
-                          style: buttonStyle),
-                    ],
-                  ),
-                )
-              ],
-            ),
     );
   }
 }
