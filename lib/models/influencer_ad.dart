462626a84068319a9d13e143407be4fe5f42f5f5
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-23 18:40:34 +0430

    InfluencerAd class created

---
+++ lib/models/influencer_ad.dart
@@ -0,0 +1,16 @@
+import 'ad.dart';
+
+class InfluencerAd {
+  String _shortLink;
+  int _clicks;
+  String _approvedAt;
+  Ad _ad;
+
+  InfluencerAd(this._shortLink, this._clicks, this._approvedAt, this._ad);
+
+
+  String get shortLink => _shortLink;
+  Ad get ad => _ad;
+  String get approvedAt => _approvedAt;
+  int get clicks => _clicks;
+}
