470edbbf5a1f4c7c5fdfbad27af68b5969443711
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-08-03 02:04:18 +0430

    deduction parameter added in IndluencerAd

--- lib/models/influencer_ad.dart
+++ lib/models/influencer_ad.dart
@@ -5,13 +5,15 @@ class InfluencerAd {
   int _clicks;
   String _approvedAt;
   Ad _ad;
+  int _deduction;
   late int income;
 
-  InfluencerAd(this._shortLink, this._clicks, this._approvedAt, this._ad);
+  InfluencerAd(this._shortLink, this._clicks, this._deduction, this._approvedAt, this._ad);
 
 
   String get shortLink => _shortLink;
   Ad get ad => _ad;
   String get approvedAt => _approvedAt;
   int get clicks => _clicks;
+  int get deduction => _deduction;
 }
