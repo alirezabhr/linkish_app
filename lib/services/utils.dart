0e77e58f58d3f077aa9e96815ca0163f21a6da9d
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-08-03 02:09:36 +0430

    all date times changed in utils.dart

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -2,7 +2,7 @@ import 'package:linkish/models/influencer_ad.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 bool isActiveAd(InfluencerAd ad) {
-  String adDateTime = ad.approvedAt.substring(0, 26);
+  String adDateTime = ad.approvedAt;
   DateTime? time = DateTime.tryParse(adDateTime);
   DateTime now = DateTime.now();
 
@@ -15,15 +15,22 @@ bool isActiveAd(InfluencerAd ad) {
 }
 
 String calculateRemainTime(String timeStr) {
-  String adDateTime = timeStr.substring(0, 26);
-  DateTime? time = DateTime.tryParse(adDateTime);
-  time = DateTime(time!.year, time.month, time.day+1, time.hour, time.minute, time.second);
+  DateTime? time = DateTime.tryParse(timeStr);
+  time = DateTime(
+    time!.year,
+    time.month,
+    time.day + 1,
+    time.hour,
+    time.minute,
+    time.second,
+  );
 
   DateTime now = DateTime.now();
   Duration diff = time.difference(now);
 
   final String hour = diff.inHours.toString().padLeft(2, '0');
-  final String minute = (diff.inMinutes - (diff.inHours*60)).toString().padLeft(2, '0');
+  final String minute =
+      (diff.inMinutes - (diff.inHours * 60)).toString().padLeft(2, '0');
   return "$hour:$minute";
 }
 
@@ -58,17 +65,35 @@ String getCurrentDateTime() {
   return currentDateTime;
 }
 
+List<String> splitDate(String dateTime) {
+  return (dateTime.split(" ")[0]).split("-");
+}
+
+List<String> splitTime(String dateTime) {
+  return (dateTime.split(" ")[1]).split(":");
+}
+
 String getNextDay(String today) {
-  int day = int.parse(today.substring(8, 10));
-  day += 1;
-  return today.substring(0,8) + day.toString() + today.substring(10);
+  List<String> date = splitDate(today);
+  List<String> time = splitTime(today);
+
+  DateTime dateTime = DateTime(
+    int.parse(date[0]),
+    int.parse(date[1]),
+    int.parse(date[2]),
+    int.parse(time[0]),
+    int.parse(time[1]),
+    int.parse(time[2]),
+  );
+  DateTime nextDay = dateTime.add(Duration(days: 1));
+  return nextDay.toString().substring(0, 19);
 }
 
 bool isNumeric(String number) {
-  for (int i=0; i<number.length; i++) {
+  for (int i = 0; i < number.length; i++) {
     try {
       int.parse(number[i]);
-    } catch(e) {
+    } catch (e) {
       return false;
     }
   }
@@ -78,9 +103,9 @@ bool isNumeric(String number) {
 String addDashCardNo(String cardNo) {
   String newStr = "";
 
-  for(int i=4; i <= cardNo.length; i+=4) {
-    newStr += cardNo.substring(i-4, i);
-    if (i!=cardNo.length) {
+  for (int i = 4; i <= cardNo.length; i += 4) {
+    newStr += cardNo.substring(i - 4, i);
+    if (i != cardNo.length) {
       newStr += ' - ';
     }
   }
